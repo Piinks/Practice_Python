d17127c27ea225a66dd159779009518e4513bd18
Kate Archer <Kea3k@mtmail.mtsu.edu>
2017-06-03 13:33:36 -0500

    simple syntax refresher

---
+++ reverseMean.py
@@ -0,0 +1,12 @@
+# R2
+
+def main():
+    entry = input().split()
+    r1 = int(entry[0])
+    mean = int(entry[1])
+
+    r2 = (mean*2)-r1
+
+    print(r2)
+
+main()
---
+++ simpleLoop.py
@@ -0,0 +1,8 @@
+# simpleLoop.py
+
+def main():
+    number = int(input())
+    for x in range (1,number+1):
+        print(x, "Abracadabra")
+
+main()
